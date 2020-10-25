18ca00ef39d97d03163ca4235d17383c40c87ca6
lawlernp <lawlernp@gmail.com>
2020-10-25 07:44:41 -0500

    added conditional render for description/image, based on a local state toggle

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -2,14 +2,37 @@ import React, { Component } from 'react';
 
 
 
-
 class GalleryItem extends Component {
+
+
+
+        state = {
+        showDesc: false
+    }
+
+    displayDesc = () => {
+        console.log(this.state.showDesc);
+        this.setState({
+            showDesc: !this.state.showDesc
+        })
+        
+    }
+
+
 
     render() {
         return(
-            <div className='image' key={this.props.item.id}> 
-                <img src={this.props.item.path} width="100" height="100"/>
-            </div>
+            <>
+                {/* conditional render using ternary operator*/}
+                <div onClick={this.displayDesc} className='image' key={this.props.item.id}> 
+                {this.state.showDesc ?
+                    <span>{this.props.item.description}</span>
+                :
+                    <img src={this.props.item.path} width="100" height="100"/>
+                }
+                </div>          
+
+            </>
         )
     }
 }
